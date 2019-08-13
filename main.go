939affee3407bc3846526a8f1a36cbad6a0fbbb2
mrsalvini <dsgmcosta@gmail.com>
2019-08-13 14:10:12 +0100

    for loop tests

--- main.go
+++ main.go
@@ -3,20 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	// var colors map[string]string
 
-	colors := make(map[string]string)
-
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#4bf745",
-	// }
-
-	colors["white"] = "#ffffff"
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
 
 	fmt.Println(colors)
+	printMap(colors)
+}
 
-	delete(colors, "white")
-
-	fmt.Println(colors)
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println(hex, "is the hex code for", color)
+	}
 }
